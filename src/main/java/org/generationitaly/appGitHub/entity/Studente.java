51f9796a76d692d026ca337824a192480f6e2877
JoelVargasMurcia <joelvargasit@gmail.com>
2022-09-22 11:02:02 +0200

    Aggiunto g&s e to string

--- src/main/java/org/generationitaly/appGitHub/entity/Studente.java
+++ src/main/java/org/generationitaly/appGitHub/entity/Studente.java
@@ -5,4 +5,35 @@ public class Studente {
 	private String codiceFiscale;
 	private String nome;
 	private String cognome;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+	public void setCodiceFiscale(String codiceFiscale) {
+		this.codiceFiscale = codiceFiscale;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	@Override
+	public String toString() {
+		return "Studente [id=" + id + ", codiceFiscale=" + codiceFiscale + ", nome=" + nome + ", cognome=" + cognome
+				+ "]";
+	}
+	
+	
 }
